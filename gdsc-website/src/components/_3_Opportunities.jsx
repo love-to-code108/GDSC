1efb05146e94c4a0c33f65856d5b64c28a8509c8
love-to-code108 <love.to.code108@gmail.com>
2023-08-17 01:57:55 +0530

    i surrender for now good night

---
+++ gdsc-website/src/components/_3_Opportunities.jsx
@@ -0,0 +1,17 @@
+import "./index.css"
+import "../../indexOutput.css"
+
+export const OpportunitiesSection = () => {
+
+
+
+    return(
+
+        <div id="Opportunities">
+            
+            <div className="OpportunitiesContainer">
+
+            </div>
+        </div>
+    );
+}
--- gdsc-website/src/main.jsx
+++ gdsc-website/src/main.jsx
@@ -8,11 +8,13 @@ import "../indexOutput.css"
 // IMPORTING ALL THE COMPONENTS BELOW 
 import { HeroSection } from './components/_1_heroSection'
 import { AboutSection } from './components/_2_aboutGdsc'
+import { OpportunitiesSection } from './components/_3_Opportunities'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <HeroSection/>
     <AboutSection/>
+    <OpportunitiesSection/>
   </React.StrictMode>,
 )
