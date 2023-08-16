96e5bd4535d5b7a5425170ea4497746f5f97ac0c
love-to-code108 <love.to.code108@gmail.com>
2023-08-16 23:40:32 +0530

    the landing page is nearly done

--- gdsc-website/src/components/_1_heroSection.jsx
+++ gdsc-website/src/components/_1_heroSection.jsx
@@ -1,7 +1,15 @@
-import "../components/index.css"
-import "../indexOutput.css"
+import "./index.css"
+import "../../indexOutput.css"
 
-// IMPORTING 
+// IMPORTING IMAGES
+import gitStraightHeroBigLine from "../assets/svg/Line 1.svg"
+import gdscLogoAnimation from "../assets/svg/google-developers-seeklogo.com.svg"
+import githubLogo from "../assets/svg/github.svg"
+import gitHead from "../assets/svg/Ellipse 1.svg"
+import gitSmallLine from "../assets/svg/Line 23.svg"
+
+
+// import { GitTag } from "./childComponents/gitTag"
 
 
 // IMPORTING GSAP ANIMATION SOFTWARE    
@@ -24,41 +32,69 @@ export const HeroSection = () => {
     // })
 
     return (
-        <div id="HeroSection_1_" className="relative flex flex-col items-center">
+        <div id="HeroSection_1_" className="flex flex-col items-center">
+
+            <div className="font-poppins absolute GDSCbig">
+                <p>GDSC</p>
+            </div>
 
 
             <div className="relative 
             UEM flex justify-center items-center
-            lg:right-[5rem] lg:top-[66%]" >
+            lg:right-[12rem] lg:top-[69%]" >
 
 
                 <p className="font-sora text-[#C3C3C3] lg_UEM
-                lg:text-[20rem] lg:font-black lg:absolute ">
+                lg:text-[20rem] lg:font-black lg:absolute drop-shadow-lg">
                     UEM</p>
 
 
                 <div className="absolute Jaipur
             flex justify-center items-center
-            lg:right-[-11rem] bottom-[1rem]">
+            lg:right-[-11rem] bottom-[1.2rem]">
 
                     <p className="font-sora text-[#515151]
-                lg:text-8xl lg:font-black lg:absolute ">
+                lg:text-8xl lg:font-black lg:absolute drop-shadow-lg">
                         JAIPUR</p>
                 </div>
 
+                {/* THE GDSC LOGO GOES BELOW */}
+                <div className="gdscLogoContainer">
+
+
+                    <img className="" src={gdscLogoAnimation} alt="" />
+                </div>
+
+
 
                 <div>
 
+                    <div className=" absolute 
+                    lg:top-[-9.6rem] lg:right-[2rem] androidGif">
 
-                <p className="font-poppins GoogleStudentDeveloperStudentClubs
+
+                        <iframe className="" src="https://giphy.com/embed/llarwdtFqG63IlqUR1">
+
+                        </iframe>
+                    </div>
+
+
+                    <p className="font-poppins GoogleStudentDeveloperStudentClubs
                 absolute
                 lg:text-4xl lg:font-black lg:top-[-3rem] lg:left-[14rem]"
-                >Google Developer Student Clubs</p>
+                    >Google Developer Student Clubs</p>
 
-                <div>
-                    <img src={} alt="" />
                 </div>
-            </div>
+
+
+                {/* THE JOIN BUTTON */}
+                <div className="absolute bg-[#4385f3] text-white flex justify-center items-center  
+                font-sora lg:font-black lg:text-5xl lg:w-[4em] lg:h-[1.4em] lg:pt-1 
+                lg:top-[-15rem] lg:left-[4rem] drop-shadow-lg">
+                    JOIN
+                </div>
+
+
 
 
             </div>
@@ -66,9 +102,67 @@ export const HeroSection = () => {
 
 
 
+            <img className="absolute top-[40%] right-[6.9rem]" src={gitStraightHeroBigLine} alt="" />
 
 
-            
+
+
+
+
+
+            {/* THE GIT BRANCH TAG */}
+            <div id="gitTag" className=" absolute flex justify-evenly items-center font-jetbrain rounded-lg
+        lg:px-8 lg:py-3 bg-white drop-shadow-lg lg:top-[4rem] lg:right-[5rem]"
+            >
+
+                <img className="
+            lg:w-[2.4rem]" src={githubLogo} alt="" />
+                <p className=" text-xl lg:ml-4">git branch GDSC</p>
+
+                {/* GIT HEAD */}
+                <img className=" absolute
+            lg:w-[2.5rem] lg:bottom-[-2.6rem] lg:right-[1rem]" src={gitHead} alt="" />
+
+                {/* GIT LINE */}
+                <img className=" absolute
+            lg:top-[6.5rem] lg:right-[1.95rem] lg:w-[0.56rem]" src={gitSmallLine} alt="" />
+
+                {/* GIT HEAD */}
+                <img className=" absolute
+            lg:w-[2.4rem] lg:bottom-[-9rem] lg:right-[1rem]" src={gitHead} alt="" />
+            </div>
+
+
+
+
+
+
+            {/* THE GIT CHECKOUT TAG */}
+            <div id="gitTag" className=" absolute flex justify-evenly items-center font-jetbrain rounded-lg
+        lg:px-8 lg:py-3 bg-white drop-shadow-lg lg:top-[17rem] lg:right-[5rem]"
+            >
+
+                <img className="
+            lg:w-[2.4rem]" src={githubLogo} alt="" />
+                <p className=" text-xl lg:ml-4">git checkout GDSC</p>
+
+                <img className=" absolute
+            lg:w-[2.5rem] lg:bottom-[-2.6rem] lg:right-[1rem]" src={gitHead} alt="" />
+
+                {/*  */}
+                <img className=" absolute
+            lg:top-[6.5rem] lg:right-[1.9rem]" src={gitSmallLine} alt="" />
+
+                {/* GIT HEAD */}
+                <img className=" absolute
+            lg:w-[2.4rem] lg:bottom-[-9rem] lg:right-[1rem]" src={gitHead} alt="" />
+
+            </div>
+
+
+
+
+
 
 
         </div>
--- gdsc-website/src/main.jsx
+++ gdsc-website/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './components/index.css'
-import "./indexOutput.css"
+
+import "../indexOutput.css"
 
 
 
@@ -11,7 +11,6 @@ import { HeroSection } from './components/_1_heroSection'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    {/* THIS IS THE HERO SECTION */}
     <HeroSection/>
   </React.StrictMode>,
 )
